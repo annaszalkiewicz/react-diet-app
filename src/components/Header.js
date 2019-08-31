3da9098b0041f27b22072045912dc65042c9d677
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-31 23:13:56 +0200

    Add toggleNavHandler() that is executed on icon click

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Header = () => {
+  const [navIsOpen, toggleNav] = useState(false);
+
+  const toggleNavHandler = () => {
+    (!navIsOpen) ? toggleNav(true) : toggleNav(false);
+  };
+
   return (
     <header className="header">
-      <svg viewBox="0 0 48 38" width="48" height="38" className="header_icon">
-        <rect y="4" width="48" height="4" class="rect-1" />
-        <rect y="17" width="48" height="4" class="rect-2" />
-        <rect y="17" width="48" height="4" class="rect-3" />
-        <rect y="30" width="48" height="4" class="rect-1" />
-      </svg>
+      {!navIsOpen && (
+        <svg
+          viewBox="0 0 48 38"
+          width="48"
+          height="38"
+          className="header_icon"
+          onClick={toggleNavHandler}
+        >
+          <rect y="4" width="48" height="4" className="rect-1" />
+          <rect y="17" width="48" height="4" className="rect-2" />
+          <rect y="17" width="48" height="4" className="rect-3" />
+          <rect y="30" width="48" height="4" className="rect-1" />
+        </svg>
+      )}
+      {navIsOpen && (
+        <svg
+          viewBox="0 0 48 38"
+          width="48"
+          height="38"
+          className="header_icon"
+          onClick={toggleNavHandler}
+        >
+          <rect y="17" width="48" height="4" className="rect-2" style={{transform: 'rotate(45deg)', transformOrigin: '50%'}} />
+          <rect y="17" width="48" height="4" className="rect-3" style={{transform: 'rotate(-45deg)', transformOrigin: '50%'}} />
+        </svg>
+      )}
     </header>
   );
 };
