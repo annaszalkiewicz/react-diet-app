ab7a9550baf21d0c2798b6754e40bb46fac0d9d0
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-31 23:56:14 +0200

    Create navigation in header

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <div>
-      Navigation
-    </div>
+    <Nav className="header_menu">
+      <NavLink to="/" className="header_menu_item">Dashbboard</NavLink>
+      <NavLink to="/recipes" className="header_menu_item">Recipes</NavLink>
+      <NavLink to="/challenge" className="header_menu_item">Challenge</NavLink>
+    </Nav>
   );
 };
 
