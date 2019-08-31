5c30fb9215a54488a673d8a78bd809a608eeabec
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-08-31 23:48:59 +0200

    Create routes: dashboard, recipes, challenge

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,36 @@
-import React from 'react';
-import '../sass/_App.scss';
-import Header from './Header';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "../sass/_App.scss";
+import Header from "./Header";
+import Dashboard from "./Dashboard";
+import Recipes from "./Recipes";
+import Challenge from "./Challenge";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
+
+      <Router>
+        <Route
+          exact
+          path={process.env.PUBLIC_URL + "/"}
+          render={() => <Dashboard />}
+        />
+
+        <Route
+          path={process.env.PUBLIC_URL + "/recipes"}
+          render={() => <Recipes />}
+        />
+
+        <Route
+          path={process.env.PUBLIC_URL + "/challenge"}
+          render={() => <Challenge />}
+        />
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Recipes = () => {
   return (
     <div>
-      
+      Recipes component
     </div>
   );
 };
