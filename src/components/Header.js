b6932b170332672337c4351aa8b75e69c5ddda7c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-01 22:10:48 +0200

    Connect with store to check if nav should be open

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,62 +1,85 @@
-import React, { useState } from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+import { isNavOpen } from "../store/actions/uiActions";
 import Nav from "./Nav";
 import User from "./User";
 
-const Header = () => {
-  const [navIsOpen, toggleNav] = useState(false);
-
-  const toggleNavHandler = () => {
-    !navIsOpen ? toggleNav(true) : toggleNav(false);
+class Header extends Component {
+  toggleNavHandler = () => {
+    this.props.toggleNav();
   };
 
-  return (
-    <header className="header">
-      <div className="header_left">
-        {!navIsOpen && (
-          <svg
-            viewBox="0 0 38 38"
-            width="38"
-            height="38"
-            className="header_left--icon"
-            onClick={toggleNavHandler}
-          >
-            <rect y="4" width="38" height="4" className="rect-1" />
-            <rect y="17" width="38" height="4" className="rect-2" />
-            <rect y="17" width="38" height="4" className="rect-3" />
-            <rect y="30" width="38" height="4" className="rect-1" />
-          </svg>
-        )}
-        {navIsOpen && (
-          <React.Fragment>
+  render() {
+    const { navIsOpen } = this.props;
+
+    return (
+      <header className="header">
+        <div className="header_left">
+          {!navIsOpen && (
             <svg
               viewBox="0 0 38 38"
               width="38"
               height="38"
               className="header_left--icon"
-              onClick={toggleNavHandler}
+              onClick={this.toggleNavHandler}
             >
-              <rect
-                y="17"
-                width="38"
-                height="4"
-                className="rect-2"
-                style={{ transform: "rotate(45deg)", transformOrigin: "50%" }}
-              />
-              <rect
-                y="17"
-                width="38"
-                height="4"
-                className="rect-3"
-                style={{ transform: "rotate(-45deg)", transformOrigin: "50%" }}
-              />
+              <rect y="4" width="38" height="4" className="rect-1" />
+              <rect y="17" width="38" height="4" className="rect-2" />
+              <rect y="17" width="38" height="4" className="rect-3" />
+              <rect y="30" width="38" height="4" className="rect-1" />
             </svg>
-            <Nav />
-          </React.Fragment>
-        )}
-      </div>
-      <User />
-    </header>
-  );
+          )}
+          {navIsOpen && (
+            <React.Fragment>
+              <svg
+                viewBox="0 0 38 38"
+                width="38"
+                height="38"
+                className="header_left--icon"
+                onClick={this.toggleNavHandler}
+              >
+                <rect
+                  y="17"
+                  width="38"
+                  height="4"
+                  className="rect-2"
+                  style={{ transform: "rotate(45deg)", transformOrigin: "50%" }}
+                />
+                <rect
+                  y="17"
+                  width="38"
+                  height="4"
+                  className="rect-3"
+                  style={{
+                    transform: "rotate(-45deg)",
+                    transformOrigin: "50%"
+                  }}
+                />
+              </svg>
+              <Nav />
+            </React.Fragment>
+          )}
+        </div>
+        <User />
+      </header>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    navIsOpen: state.uiReducer.isNavOpen
+  };
 };
 
-export default Header;
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleNav: () => dispatch(isNavOpen())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Header);
