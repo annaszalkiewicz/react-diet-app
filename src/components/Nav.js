95d69e18979c9559075b708754a8b94498b5ab29
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-01 13:20:09 +0200

    Fix loading and crushing issue Replace Nav to nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,11 +3,11 @@ import { NavLink } from "react-router-dom";
 
 const Nav = () => {
   return (
-    <Nav className="header_menu">
+    <nav className="header_menu">
       <NavLink to="/" className="header_menu_item">Dashbboard</NavLink>
       <NavLink to="/recipes" className="header_menu_item">Recipes</NavLink>
       <NavLink to="/challenge" className="header_menu_item">Challenge</NavLink>
-    </Nav>
+    </nav>
   );
 };
 
