06a7939e069aa9a126f21a6e641166ce542fc8de
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-01 14:10:05 +0200

    Redirect to dashboard if user is logged in

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,9 @@
-import React from "react";
+import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import { connect } from "react-redux";
+import { Redirect } from 'react-router';
+ 
+import { logIn } from "../store/actions/authActions";
 
 import "../sass/_App.scss";
 import Header from "./Header";
@@ -8,33 +12,64 @@ import Recipes from "./Recipes";
 import Challenge from "./Challenge";
 import Home from "./Home";
 
-const App = () => {
-  return (
-    <div className="App">
-      <Header />
+class App extends Component {
 
-      <Route
-        exact
-        path={process.env.PUBLIC_URL + "/"}
-        render={() => <Home />}
-      />
+  componentDidMount = () => {
+    setTimeout(() => {
+      this.props.onLogIn();
+    }, 5000);
+  };
 
-      <Route
-        path={process.env.PUBLIC_URL + "/dashboard"}
-        render={() => <Dashboard />}
-      />
+  render() {
+    return (
+      <div className="App">
+        <Header />
 
-      <Route
-        path={process.env.PUBLIC_URL + "/recipes"}
-        render={() => <Recipes />}
-      />
-
-      <Route
-        path={process.env.PUBLIC_URL + "/challenge"}
-        render={() => <Challenge />}
-      />
-    </div>
-  );
+        <Route 
+          exact 
+          path={process.env.PUBLIC_URL + "/"} 
+          render={() => (
+          this.props.isLogin ? (
+            <Redirect to="/dashboard"/>
+          ) : (
+            <Home />
+          )
+        )}/>
+  
+        <Route
+          path={process.env.PUBLIC_URL + "/dashboard"}
+          render={() => <Dashboard />}
+        />
+  
+        <Route
+          path={process.env.PUBLIC_URL + "/recipes"}
+          render={() => <Recipes />}
+        />
+  
+        <Route
+          path={process.env.PUBLIC_URL + "/challenge"}
+          render={() => <Challenge />}
+        />
+      </div>
+    );
+  }
+  
 };
 
-export default App;
+const mapStateToProps = state => {
+  return {
+    isLogin: state.authReducer.isLogin
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    onLogIn: () => dispatch(logIn())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
+
