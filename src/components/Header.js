f3b57e189205a17a23b1f892eb4290194e5dc6e3
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-01 16:06:59 +0200

    Add styling to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,47 +10,50 @@ const Header = () => {
 
   return (
     <header className="header">
-      {!navIsOpen && (
-        <svg
-          viewBox="0 0 48 38"
-          width="48"
-          height="38"
-          className="header_icon"
-          onClick={toggleNavHandler}
-        >
-          <rect y="4" width="48" height="4" className="rect-1" />
-          <rect y="17" width="48" height="4" className="rect-2" />
-          <rect y="17" width="48" height="4" className="rect-3" />
-          <rect y="30" width="48" height="4" className="rect-1" />
-        </svg>
-      )}
-      {navIsOpen && (
-        <React.Fragment>
+      <div className="header_left">
+        {!navIsOpen && (
           <svg
-            viewBox="0 0 48 38"
-            width="48"
+            viewBox="0 0 38 38"
+            width="38"
             height="38"
-            className="header_icon"
+            className="header_left--icon"
             onClick={toggleNavHandler}
           >
-            <rect
-              y="17"
-              width="48"
-              height="4"
-              className="rect-2"
-              style={{ transform: "rotate(45deg)", transformOrigin: "50%" }}
-            />
-            <rect
-              y="17"
-              width="48"
-              height="4"
-              className="rect-3"
-              style={{ transform: "rotate(-45deg)", transformOrigin: "50%" }}
-            />
+            <rect y="4" width="38" height="4" className="rect-1" />
+            <rect y="17" width="38" height="4" className="rect-2" />
+            <rect y="17" width="38" height="4" className="rect-3" />
+            <rect y="30" width="38" height="4" className="rect-1" />
           </svg>
-          <Nav />
-        </React.Fragment>
-      )}
+        )}
+        {navIsOpen && (
+          <React.Fragment>
+            <svg
+              viewBox="0 0 38 38"
+              width="38"
+              height="38"
+              className="header_left--icon"
+              onClick={toggleNavHandler}
+            >
+              <rect
+                y="17"
+                width="38"
+                height="4"
+                className="rect-2"
+                style={{ transform: "rotate(45deg)", transformOrigin: "50%" }}
+              />
+              <rect
+                y="17"
+                width="38"
+                height="4"
+                className="rect-3"
+                style={{ transform: "rotate(-45deg)", transformOrigin: "50%" }}
+              />
+            </svg>
+            <Nav />
+          </React.Fragment>
+        )}
+      </div>
+      <div className="header_right"></div>
     </header>
   );
 };
