a1925c0fb2c3e14cd0724c07cf1f880a0ce6e5f6
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-01 22:47:51 +0200

    Connect with store to open/close Nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,14 +1,37 @@
-import React from "react";
+import React, { Component } from "react";
 import { NavLink } from "react-router-dom";
+import { connect } from "react-redux";
 
-const Nav = () => {
-  return (
-    <nav className="header_menu">
-      <NavLink exact to="/dashboard" className="header_menu--item">Dashboard</NavLink>
-      <NavLink exact to="/recipes" className="header_menu--item">Recipes</NavLink>
-      <NavLink exact to="/challenge" className="header_menu--item">Challenge</NavLink>
-    </nav>
-  );
+import { isNavOpen } from "../store/actions/uiActions";
+
+class Nav extends Component {
+
+  render() {
+   
+    return (
+      <nav className="header_menu" onClick={this.props.toggleNav}>
+        <NavLink exact to="/dashboard" className="header_menu--item">Dashboard</NavLink>
+        <NavLink exact to="/recipes" className="header_menu--item">Recipes</NavLink>
+        <NavLink exact to="/challenge" className="header_menu--item">Challenge</NavLink>
+      </nav>
+    );
+  }
+  
 };
 
-export default Nav;
+const mapStateToProps = state => {
+  return {
+    navIsOpen: state.uiReducer.isNavOpen
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    toggleNav: () => dispatch(isNavOpen())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Nav);
