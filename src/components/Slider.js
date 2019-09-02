0fa19c0911e9320b34ad1547c8fde8a3e62a3d21
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-03 00:42:40 +0200

    Edit slider content

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -7,23 +7,24 @@ class Slider extends Component {
     return (
       <section className="slider">
         <div className="slider_container">
-          <h1 className="slider_title">Week 1</h1>
-          <h1 className="slider_title">Week 2</h1>
-          <h1 className="slider_title">Week 3</h1>
-          <h1 className="slider_title">Week 4</h1>
-          <h1 className="slider_title">Week 5</h1>
-          <h1 className="slider_title">Week 6</h1>
-          <h1 className="slider_title">Week 7</h1>
-          <h1 className="slider_title">Week 8</h1>
-          <h1 className="slider_title">Week 9</h1>
-          <h1 className="slider_title">Week 10</h1>
-          <h1 className="slider_title">Week 11</h1>
-          <h1 className="slider_title">Week 12</h1>
-
-          <button className="button_primary prev">
+          <button className="button_prev button_primary">
             <Icon path={mdiChevronLeft} size={1} color="#828282" />
           </button>
-          <button className="button_primary next">
+
+          <h1 className="slider_item slider_item--active">Week 1</h1>
+          <h1 className="slider_item">Week 2</h1>
+          <h1 className="slider_item">Week 3</h1>
+          <h1 className="slider_item">Week 4</h1>
+          <h1 className="slider_item">Week 5</h1>
+          <h1 className="slider_item">Week 6</h1>
+          <h1 className="slider_item">Week 7</h1>
+          <h1 className="slider_item">Week 8</h1>
+          <h1 className="slider_item">Week 9</h1>
+          <h1 className="slider_item">Week 10</h1>
+          <h1 className="slider_item">Week 11</h1>
+          <h1 className="slider_item">Week 12</h1>
+
+          <button className="button_next button_primary">
             <Icon path={mdiChevronRight} size={1} color="#828282" />
           </button>
         </div>
