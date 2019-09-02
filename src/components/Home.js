c210f3f6151f1b2397138db6613aa1aee73cd25c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-02 21:41:56 +0200

    Replace text with loading icon

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,19 @@
 import React from "react";
 
+import { Icon } from '@mdi/react';
+import { mdiReload } from '@mdi/js';
+
 const Home = () => {
   return (
     <div className="home_container">
-      <p className="home_container--text">Redirecting to dashboard...</p>
+      <p className="home_container--text">
+        <Icon 
+          path={mdiReload}
+          size={2}
+          color="#ff811c"
+          spin={1}s
+        />
+      </p>
     </div>
   );
 };
