697456a9b282fba3808e00f06a1eda95b70b5b4e
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-03 23:37:13 +0200

    Create MealPlan component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Timeline from "./Timeline";
 import Slider from "./Slider";
+import MealPlan from "./MealPlan";
 
 const Dashboard = () => {
   return (
     <div className="dashboard_container">
       <Timeline />
       <Slider />
+      <MealPlan />
     </div>
   );
 };
---
+++ src/components/MealPlan.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import data from '../data/db.json';
+
+class MealPlan extends Component {
+  render() {
+    return (
+      <section className="mealPlan">
+        
+      </section>
+    )
+  }
+}
+
+export default MealPlan;
