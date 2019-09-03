b5bbd585304bc24a8c1922c22d79993940e2f459
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-03 18:02:52 +0200

    Create timeline dynamically

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -8,42 +8,17 @@ class Timeline extends Component {
       <section className="timeline">
         <h3 className="timeline_heading">Your 12 weeks progress</h3>
         <div className="timeline_container">
-          <div className="timeline_item">
-            <p>1</p>
-          </div>
-          <div className="timeline_item">
-            <p>2</p>
-          </div>
-          <div className="timeline_item">
-            <p>3</p>
-          </div>
-          <div className="timeline_item">
-            <p>4</p>
-          </div>
-          <div className="timeline_item">
-            <p>5</p>
-          </div>
-          <div className="timeline_item">
-            <p>6</p>
-          </div>
-          <div className="timeline_item">
-            <p>7</p>
-          </div>
-          <div className="timeline_item">
-            <p>8</p>
-          </div>
-          <div className="timeline_item">
-            <p>9</p>
-          </div>
-          <div className="timeline_item">
-            <p>10</p>
-          </div>
-          <div className="timeline_item">
-            <p>11</p>
-          </div>
-          <div className="timeline_item">
-            <p>12</p>
-          </div>
+
+          {this.props.weeks.map(item => {
+            return (
+              <div className="timeline_item" key={item}>
+                <div className="timeline_line"></div>
+                <div className="timeline_circle"></div>
+                <p>{item}</p>
+              </div>
+            )
+          })}
+        
         </div>
       </section>
     );
