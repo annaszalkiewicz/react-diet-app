664c505a49fb2a8aa88f249fc9162a60674b8d71
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-03 16:12:59 +0200

    Add setPlanDuration action

--- src/store/actions/actionTypes.js
+++ src/store/actions/actionTypes.js
@@ -1,3 +1,4 @@
 export const LOG_IN = 'LOG_IN';
 export const IS_OPEN = 'IS_OPEN';
 export const SET_CURRENT_WEEK = 'SET_CURRENT_WEEK';
+export const SET_PLAN_DURATION = 'SET_PLAN_DURATION';
--- src/store/actions/userActions.js
+++ src/store/actions/userActions.js
@@ -1,7 +1,13 @@
-import { SET_CURRENT_WEEK } from './actionTypes';
+import { SET_CURRENT_WEEK, SET_PLAN_DURATION } from './actionTypes';
 
 export const setCurrentWeek = () => {
   return {
     type: SET_CURRENT_WEEK
+  }
+}
+
+export const setPlanDuration = () => {
+  return {
+    type: SET_PLAN_DURATION
   }
 }
