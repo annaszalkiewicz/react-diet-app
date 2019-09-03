4a3f23c10948702222b49af60f827ce08acd71b0
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-03 18:50:02 +0200

    Fix timeline stayling depending on current week

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,24 +1,41 @@
 import React, { Component } from "react";
-import { connect } from 'react-redux';
+import { connect } from "react-redux";
 
 class Timeline extends Component {
-
   render() {
+    const { currentWeek } = this.props;
     return (
       <section className="timeline">
         <h3 className="timeline_heading">Your 12 weeks progress</h3>
         <div className="timeline_container">
-
           {this.props.weeks.map(item => {
+            console.log(item===currentWeek);
+            
             return (
               <div className="timeline_item" key={item}>
-                <div className="timeline_line"></div>
-                <div className="timeline_circle"></div>
+                <div
+                  className="timeline_line"
+                  style={{
+                    display: item === 1 ? "none" : "block",
+                    background: item < currentWeek ? "#b3c304" : "#dbdbdb"
+                  }}
+                ></div>
+                <div
+                  className="timeline_circle"
+                  style={{
+                    background:
+                      (item < currentWeek)
+                        ? "#b3c304"
+                        : (item === currentWeek)
+                        ? "#fff"
+                        : "#dbdbdb",
+                    border: item === currentWeek ? "solid 2px #dbdbdb" : null
+                  }}
+                ></div>
                 <p>{item}</p>
               </div>
-            )
+            );
           })}
-        
         </div>
       </section>
     );
@@ -29,7 +46,10 @@ const mapStateToProps = state => {
   return {
     currentWeek: state.userReducer.currentWeek,
     weeks: state.userReducer.weeks
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, null)(Timeline);
+export default connect(
+  mapStateToProps,
+  null
+)(Timeline);
