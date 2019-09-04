4b8a127d31d77c16784a957bc0a7ce7154857cd9
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-04 16:56:17 +0200

    Add styling to mealPlan container and hours

--- src/components/MealPlan.js
+++ src/components/MealPlan.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 
-import { getMealPlan } from '../store/actions/userActions';
+import { getMealPlan } from "../store/actions/userActions";
 
 class MealPlan extends Component {
-
   componentDidMount = () => {
     this.props.getMealPlan();
     console.log(this.props.mealPlan);
-  }
+  };
 
   render() {
     return (
@@ -16,7 +15,11 @@ class MealPlan extends Component {
         <div className="mealPlan_container">
           <div className="mealPlan_hours">
             {this.props.mealPlan.mealHours.map(hour => {
-              return <div className="mealPlan_hour" key={hour}>{hour}</div>;
+              return (
+                <div className="mealPlan_hour" key={hour}>
+                  {hour}
+                </div>
+              );
             })}
           </div>
         </div>
