e09b71fc1911a3cbfbce195779b66bf824174c5c
annaszalkiewicz <anna@mywebgraphicdesign.com>
2019-09-06 00:36:19 +0200

    Display buttons depending on protein options state

--- src/components/ProteinOptions.js
+++ src/components/ProteinOptions.js
@@ -1,48 +1,134 @@
-import React, { Component } from 'react';
-import vegetable from '../assets/img/vegetables.png';
-import vagetableDisabled from '../assets/img/vegetables-disabled.png';
-import dairy from '../assets/img/dairy.png';
-import dairyDisabled from '../assets/img/dairy-disabled.png';
-import beef from '../assets/img/beef.png';
-import beefDisabled from '../assets/img/beef-disabled.png';
-import chicken from '../assets/img/chicken.png';
-import chickenDisabled from '../assets/img/chicken-disabled.png';
-import fish from '../assets/img/fish.png';
-import fishDisabled from '../assets/img/fish-disabled.png';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
+import { setProteinOptions } from "../store/actions/userActions";
+
+import vegetable from "../assets/img/vegetables.png";
+import vagetableDisabled from "../assets/img/vegetables-disabled.png";
+import dairy from "../assets/img/dairy.png";
+import dairyDisabled from "../assets/img/dairy-disabled.png";
+import beef from "../assets/img/beef.png";
+import beefDisabled from "../assets/img/beef-disabled.png";
+import chicken from "../assets/img/chicken.png";
+import chickenDisabled from "../assets/img/chicken-disabled.png";
+import fish from "../assets/img/fish.png";
+import fishDisabled from "../assets/img/fish-disabled.png";
 
 class ProteinOptions extends Component {
+  componentDidMount = () => {
+    this.props.setProteinOptions();
+  };
+
+  clickHandler = e => {
+    if (e.target.classList.contains("options_button")) {
+    }
+    return;
+  };
   render() {
+    const { mealPlan } = this.props;
     return (
       <section className="options">
-        <h3 className="options_header">
-          Select your protein options
-        </h3>
-        <div className="options_buttons">
+        <h3 className="options_header">Select your protein options</h3>
+        <div className="options_buttons" onClick={e => this.clickHandler(e)}>
           <button className="options_button">
-            <img src={vegetable} alt="Vegetable option enabled" className="options_vegetable--active" />
-            <img src={vagetableDisabled} alt="Vegetable option disabled" className="options_vegetable--inactive" />
+            {mealPlan.proteinOptions.vegetables && (
+              <img
+                src={vegetable}
+                alt="Vegetable option enabled"
+                className="options_vegetable--active"
+              />
+            )}
+            {!mealPlan.proteinOptions.vegetables && (
+              <img
+                src={vagetableDisabled}
+                alt="Vegetable option disabled"
+                className="options_vegetable--inactive"
+              />
+            )}
           </button>
           <button className="options_button">
-            <img src={dairy} alt="Dairy option enabled" className="options_vegetable--active" />
-            <img src={dairyDisabled} alt="Dairy option disabled" className="options_vegetable--inactive" />
+            {mealPlan.proteinOptions.dairy && (
+              <img
+                src={dairy}
+                alt="Dairy option enabled"
+                className="options_vegetable--active"
+              />
+            )}
+            {!mealPlan.proteinOptions.dairy && (
+              <img
+                src={dairyDisabled}
+                alt="Dairy option disabled"
+                className="options_vegetable--inactive"
+              />
+            )}
           </button>
           <button className="options_button">
-            <img src={beef} alt="Beef option enabled" className="options_vegetable--active" />
-            <img src={beefDisabled} alt="Beef option disabled" className="options_vegetable--inactive" />
+            {mealPlan.proteinOptions.beef && (
+              <img
+                src={beef}
+                alt="Beef option enabled"
+                className="options_vegetable--active"
+              />
+            )}
+            {!mealPlan.proteinOptions.beef && (
+              <img
+                src={beefDisabled}
+                alt="Beef option disabled"
+                className="options_vegetable--inactive"
+              />
+            )}
           </button>
           <button className="options_button">
-            <img src={fish} alt="Fish option enabled" className="options_vegetable--active" />
-            <img src={fishDisabled} alt="Fish option disabled" className="options_vegetable--inactive" />
+            {mealPlan.proteinOptions.fish && (
+              <img
+                src={fish}
+                alt="Fish option enabled"
+                className="options_vegetable--active"
+              />
+            )}
+            {!mealPlan.proteinOptions.fish && (
+              <img
+                src={fishDisabled}
+                alt="Fish option disabled"
+                className="options_vegetable--inactive"
+              />
+            )}
           </button>
           <button className="options_button">
-            <img src={chicken} alt="Chicken option enabled" className="options_vegetable--active" />
-            <img src={chickenDisabled} alt="Chicken option disabled" className="options_vegetable--inactive" />
+            {mealPlan.proteinOptions.chicken && (
+              <img
+                src={chicken}
+                alt="Chicken option enabled"
+                className="options_vegetable--active"
+              />
+            )}
+            {!mealPlan.proteinOptions.chicken && (
+              <img
+                src={chickenDisabled}
+                alt="Chicken option disabled"
+                className="options_vegetable--inactive"
+              />
+            )}
           </button>
         </div>
       </section>
-    )
+    );
   }
 }
 
-export default ProteinOptions;
+const mapStateToProps = state => {
+  return {
+    mealPlan: state.userReducer.mealPlan
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    setProteinOptions: () => dispatch(setProteinOptions())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ProteinOptions);
